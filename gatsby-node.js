c0f62568eebe166d052e9513f930e46c768f9d93
Ola <ola@olavea.com>
2022-01-26 11:15:35 +0100

    ship: show-1-video-pow-of-G.i.G.G.L.E.S-#9

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,22 +2,23 @@
 const ID_GiGGLES = `eRTJPIa39a4`;
 const axios = require("axios");
 
-async function prepPugNode({actions, createNodeId, createContentDigest}) {
-    const { data } = await axios.get("https://www.youtube.com/oembed",
-        {params: {
-        url: `https://youtu.be/${ID_GiGGLES}`,
-        maxwidth: 356
-    }});
-    actions.createNode({
-        ...data,
-        id: createNodeId(`pug-${ID_GiGGLES}`),
-        internal: {
-            contentDigest: createContentDigest(ID_GiGGLES),
-            type: `pugNode`
-        }
-    })
-};
+async function prepPugNode({ actions, createNodeId, createContentDigest }) {
+  const { data } = await axios.get("https://www.youtube.com/oembed", {
+    params: {
+      url: `https://youtu.be/${ID_GiGGLES}`,
+      maxwidth: 356,
+    },
+  });
+  actions.createNode({
+    ...data,
+    id: createNodeId(`pug-${ID_GiGGLES}`),
+    internal: {
+      contentDigest: createContentDigest(ID_GiGGLES),
+      type: `pugNode`,
+    },
+  });
+}
 
 exports.sourceNodes = async (params) => {
-    await Promise.all([prepPugNode(params)]);
+  await Promise.all([prepPugNode(params)]);
 };
--- src/components/video-layout.js
+++ src/components/video-layout.js
@@ -6,15 +6,17 @@ export default function VideoLayout({ title, html, author_name }) {
     <>
       <article>
         <h1 className="mark">{title}</h1>
-          <div
-            className="video-container"
-            dangerouslySetInnerHTML={{ __html: html }}
-          />
+        <div
+          className="video-container"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
         <h2 className="mark">Made with 💜 by: {author_name} 👑 </h2>
+        <p>
+          Click fullscreen on the video. The tiny frame in the bottom right
+          coner. To the right to of "YOUTUBE".
+        </p>
       </article>
-      <div>
-
-      </div>
+      <div></div>
     </>
   );
-};
+}
