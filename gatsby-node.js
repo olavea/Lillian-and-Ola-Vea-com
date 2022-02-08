a2dc5628db9e752c7bae1d5174002206edf2edc9
Ola <ola@olavea.com>
2022-02-08 09:33:40 +0100

    fix: renaming and deleting comments #20

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,15 +1,6 @@
 // POW!-website/gatsby-node.js
-
-//                 Troya Goatsby and Lizabeth Catsby are
-//                 Baking pages
-//                 with Cap'n Granny Sharksby's
-//                 createPages hook
-async function bakeMarkdownNodesIntoGoodies({ graphql, actions }) {
-  // console.log("Captain Granny Sharksbys createPages hook💀");
-  //              1. bakingSong = Troya Goatsby and Lizabeth Catsby sings
-  //                 the bakingSong by Cap'n Granny Sharksby 🦢
-  const bakingSong = require.resolve("./src/templates/bakingSong.js");
-  //              2. supplies: allMarkdownRemark
+async function bakeMarkdownNodesIntoPages({ graphql, actions }) {
+  const pageTemplate = require.resolve("./src/templates/pageTemplate.js");
   const { data } = await graphql(`
     {
       supplies: allMarkdownRemark {
@@ -19,37 +10,17 @@ async function bakeMarkdownNodesIntoGoodies({ graphql, actions }) {
       }
     }
   `);
-  //             3. Loop over the nodes and for each create a page
-  data.supplies.nodes.forEach((ahoyGoodie) => {
-    //                  console.log('🎩', ahoyGoodie.id);
-    // // //              A. – B. – C. – (minus DSG 🎩)
+  data.supplies.nodes.forEach((node) => {
     actions.createPage({
-      // //              A. «Ahoy! Goodie?!»
-      // //                 Cap'n Foxby shouts and embarks. 🦊
-      path: `/md/${ahoyGoodie.id}`,
-      // //              B. Troya Goatsby sings badly
-      // //                 and bakes baby sharks. 🦈
-      component: bakingSong,
-      // //              C. Catsby looks tasty
-      // //                 Foxby gets hungry for kitten. 🐯
+      path: `/md/${node.id}`,
+      component: pageTemplate,
       context: {
-        catsby: ahoyGoodie.id,
+        catsby: node.id,
         fox: "Catsby looks tasty I getz hungry for kitten",
       },
-      // // //              D. Don't Show Goodies to Foxby
-      // // //              and maybe get bitten.
-      // // //              catsby build and look for _ _ _ in terminal 💀
-      // //         defer: index + 1 > 2,
-      // // //              D. Data tree 🤖🌲
-      // // //              E. Every node you must sort 💰
-      // // //              F. Fields Forever `fields: ,` 🍓
-      // // //              E. forEach index
-      // // //              R. RRR! defer: index + 1 > 2 🎩
     });
   });
 }
-exports.createPages = async (params) => {
-  // create pages dynamically from any data source like for example see below:
-  // wait for all promises to be resolved before finishing this function
-  await Promise.all([bakeMarkdownNodesIntoGoodies(params)]);
+exports.createPages = async (gatsbyUtils) => {
+  await Promise.all([bakeMarkdownNodesIntoPages(gatsbyUtils)]);
 };
