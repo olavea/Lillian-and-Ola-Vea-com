b284b9bd9ece55eeedf3d80abdb5931d1979e341
Ola <ola@olavea.com>
2022-02-19 09:50:24 +0100

    fix: create only markdown pages #34

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,6 +12,42 @@ async function slugifyMarkdownRemarkNode({ actions, node, getNode }) {
   }
 }
 
+// POW!-website/gatsby-node.js
+async function bakeMarkdownNodesIntoPages({ graphql, actions }) {
+  //  only create pages for markdown pages and not sections.
+  // To know the difference between a markdown page and section we can decide that
+  // only index.md qualify as a page. Everything else is a markdown section content.
+  const { data } = await graphql(`
+    {
+      supplies: allMarkdownRemark(
+        filter: { fields: { slug: { eq: "/index/" } } }
+      ) {
+        nodes {
+          id
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  `);
+  console.log(data.supplies);
+  const pageTemplate = require.resolve("./src/templates/pageTemplate.js");
+  data.supplies.nodes.forEach((node) => {
+    actions.createPage({
+      path: node.fields.slug,
+      component: pageTemplate,
+      context: {
+        catsby: node.id,
+      },
+    });
+  });
+}
+
 exports.onCreateNode = async (gatsbyUtils) => {
   await Promise.all([slugifyMarkdownRemarkNode(gatsbyUtils)]);
+};
+
+exports.createPages = async (gatsbyUtils) => {
+  await Promise.all([bakeMarkdownNodesIntoPages(gatsbyUtils)]);
 };
