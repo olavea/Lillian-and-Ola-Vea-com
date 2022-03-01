cf9289b16aa7e975842e6eb989c188c0e544c8a7
Ola <ola@olavea.com>
2022-03-01 08:56:47 +0100

    Add support for Newsletter Section #19

--- src/templates/pageTemplate.js
+++ src/templates/pageTemplate.js
@@ -4,6 +4,10 @@ import { graphql, Link } from "gatsby";
 export default function PageTemplate({ data = {} }) {
   const { frontmatter, html } = data.markdownRemark || {};
   const { title, sections } = frontmatter || {};
+  function createEmail(event) {
+    event.preventDefault();
+    alert("POW! You've got mail 📧 📫");
+  }
   return (
     <>
       <div className="container">
@@ -13,11 +17,41 @@ export default function PageTemplate({ data = {} }) {
           const { title, body } = section || {};
           const { html } = body?.childMarkdownRemark || {};
           const { path, label } = section.cta || {};
+          const { subtitle, form } = section || {};
           return (
             <section>
               {title && <h2>{title}</h2>}
               {html && <div dangerouslySetInnerHTML={{ __html: html }} />}
               {path && label && <Link to={path}>{label}</Link>}
+              {subtitle && form && (
+                <form
+                  onSubmit={createEmail}
+                  action="https://forms.userlist.com/b199b263-3262-435f-a9bc-96a12aa9955d/submissions"
+                  method="POST"
+                  acceptCharset="UTF-8"
+                >
+                  <fieldset>
+                    <label htmlFor="fields_first_name">First name </label>
+                    <input
+                      type="text"
+                      id="fields_first_name"
+                      name="fields[first_name]"
+                    />
+                  </fieldset>
+
+                  <fieldset>
+                    <label htmlFor="fields_email">Email </label>
+                    <input
+                      type="text"
+                      id="fields_email"
+                      name="fields[email]"
+                      required
+                    />
+                  </fieldset>
+
+                  <button type="submit">Submit</button>
+                </form>
+              )}
             </section>
           );
         })}
@@ -27,13 +61,15 @@ export default function PageTemplate({ data = {} }) {
 }
 
 export const query = graphql`
-  query ($catsbyId: String) {
+  query($catsbyId: String) {
     markdownRemark(id: { eq: $catsbyId }) {
       html
       frontmatter {
         title
         sections {
           title
+          subtitle
+          form
           body {
             childMarkdownRemark {
               html
