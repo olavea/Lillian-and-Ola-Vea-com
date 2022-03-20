dac3585eb6ddbd9bff99e97be0c22b7814a7b617
Benedicte Raae <raae@raae.codes>
2022-03-21 01:44:27 +0200

    fix(newsletter): remove onSubmit handler

--- src/components/newsletter-form.js
+++ src/components/newsletter-form.js
@@ -2,17 +2,18 @@ import React from "react";
 import { Button, TextField } from "@mui/material";
 import { Box } from "@mui/system";
 
-export const NewsletterForm = ({ sx, ...props }) => {
-  function createEmail(event) {
-    event.preventDefault();
-    alert("POW! You've got mail 📧 📫");
-  }
+const IS_PROD = process.env.NODE_ENV === "production";
 
+const USERLIST_KEY = IS_PROD
+  ? "d21dbf05-1525-4323-a256-327355066f92"
+  : "b199b263-3262-435f-a9bc-96a12aa9955d";
+const ACTION = `https://forms.userlist.com/${USERLIST_KEY}/submissions`;
+
+export const NewsletterForm = ({ sx, action = ACTION, ...props }) => {
   return (
     <Box
       component="form"
-      // onSubmit={createEmail}
-      action="https://forms.userlist.com/b199b263-3262-435f-a9bc-96a12aa9955d/submissions"
+      action={action}
       method="POST"
       acceptCharset="UTF-8"
       sx={{
@@ -28,7 +29,6 @@ export const NewsletterForm = ({ sx, ...props }) => {
         id="fields_first_name"
         name="fields[first_name]"
         sx={{ mb: 2 }}
-        required
         fullWidth
       />
       <TextField
